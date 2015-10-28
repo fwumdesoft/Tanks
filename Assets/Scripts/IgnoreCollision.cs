c110b8e611084c6ea0da8683a2283be2c4098f10
proGrammer5 <jasoncarrete5@gmail.com>
2015-10-27 23:58:30 -0400

    Initial Commit

---
+++ Assets/Scripts/IgnoreCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgnoreCollision : MonoBehaviour
+{
+	public Collider otherCollider;
+
+	void Start()
+	{
+		Physics.IgnoreCollision(GetComponent<Collider>(), otherCollider);
+	}
+}
---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankController : MonoBehaviour
+{
+	public float turretRotationSpeed = 10f;
+	public float tankRotationSpeed = 10f;
+
+	Rigidbody rb;
+	Transform turret, barrel;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		turret = transform.Find("Turret");
+		barrel = turret.transform.Find("Turret Barrel");
+	}
+	
+	void Update()
+	{
+		float turretRotY = Input.GetAxis("Mouse X") * turretRotationSpeed * Time.deltaTime;
+		float barrelRotX = -Input.GetAxis("Mouse Y") * turretRotationSpeed * Time.deltaTime;
+		turret.Rotate(0, turretRotY, 0);
+		barrel.Rotate(barrelRotX, 0, 0);
+		Mathf.Clamp(barrel.rotation.x, 50f, 95f);
+		Camera.main.transform.RotateAround(turret.position, Vector3.up, turretRotY);
+	}
+}
