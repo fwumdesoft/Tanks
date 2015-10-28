a81491e7a30a33c4fb75596c0a9c23c6627178b6
Jason C <jasoncarrete5@gmail.com>
2015-10-28 15:11:31 -0400

    sloppy tank movement

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -3,8 +3,16 @@ using System.Collections;
 
 public class TankController : MonoBehaviour
 {
-	public float turretRotationSpeed = 10f;
-	public float tankRotationSpeed = 10f;
+	[System.Serializable]
+	public struct TurretProperties
+	{
+		public float xRotationSpeed;
+		public float yRotationSpeed;
+	}
+
+	public float tankMoveSpeed = 10f;
+	public float yRotationSpeed = 10f;
+	public TurretProperties tProps;
 
 	Rigidbody rb;
 	Transform turret, barrel;
@@ -13,15 +21,23 @@ public class TankController : MonoBehaviour
 	{
 		rb = GetComponent<Rigidbody>();
 		turret = transform.Find("Turret");
-		barrel = turret.transform.Find("Turret Barrel");
+		barrel = turret.Find("Turret Barrel");
 	}
-	
+
 	void Update()
 	{
-		float turretRotY = Input.GetAxis("Mouse X") * turretRotationSpeed * Time.deltaTime;
-		float barrelRotX = -Input.GetAxis("Mouse Y") * turretRotationSpeed * Time.deltaTime;
+		float turretRotY = Input.GetAxis("Mouse X") * tProps.yRotationSpeed * Time.deltaTime;
 		turret.Rotate(0, turretRotY, 0);
-		barrel.Rotate(barrelRotX, 0, 0);
-		Camera.main.transform.RotateAround(turret.position, Vector3.up, turretRotY);
+	}
+
+	void FixedUpdate()
+	{
+		//Move forward and backward
+		float move = Input.GetAxis("Vertical") * tankMoveSpeed;
+		rb.AddForce(transform.forward * move, ForceMode.Impulse);
+//		Debug.Log(rb.velocity.ToString());
+
+		float tankRotate = Input.GetAxis("Horizontal") * yRotationSpeed;
+		transform.Rotate(transform.up * tankRotate);
 	}
 }
