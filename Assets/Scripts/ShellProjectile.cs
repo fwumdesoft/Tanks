79ae5c7a5f5292a69ada9fe2863fa72b2c15e3df
proGrammer5 <jasoncarrete5@gmail.com>
2015-10-28 23:53:12 -0400

    Begun work on shell

---
+++ Assets/Scripts/ShellProjectile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShellProjectile : MonoBehaviour
+{
+	Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	public void SetForce(Vector3 force)
+	{
+		rb.AddForce(force, ForceMode.Impulse);
+	}
+	
+	void FixedUpdate()
+	{
+		Vector3 rot = rb.velocity;
+		rot.Normalize();
+		transform.rotation = Quaternion.Euler(rot.x, rot.y, rot.z);
+	}
+}
--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -8,14 +8,27 @@ public class TankController : MonoBehaviour
 	{
 		public float xRotationSpeed;
 		public float yRotationSpeed;
+		public float xMinRotation;
+		public float xMaxRotation;
 	}
 
+	[System.Serializable]
+	public struct ShellProperties
+	{
+		public Vector3 initialMagnitude;
+		public Vector3 deltaMagnitude;
+		public float maxMagnitude;
+	}
+
+	public GameObject shellPrefab;
+	public ShellProperties shellProperties;
 	public float tankMoveSpeed = 10f;
 	public float yRotationSpeed = 10f;
-	public TurretProperties tProps;
+	public TurretProperties turretProperties;
 
 	Rigidbody rb;
 	Transform turret, barrel;
+	Vector3 shellForce;
 
 	void Start()
 	{
@@ -26,18 +39,37 @@ public class TankController : MonoBehaviour
 
 	void Update()
 	{
-		float turretRotY = Input.GetAxis("Mouse X") * tProps.yRotationSpeed * Time.deltaTime;
+		float turretRotY = Input.GetAxis("Mouse X") * turretProperties.yRotationSpeed * Time.deltaTime;
 		turret.Rotate(0, turretRotY, 0);
+		Camera.main.transform.RotateAround(turret.position, turret.up, turretRotY);
+
+		float barrelRotX = -Input.GetAxis("Mouse Y") * turretProperties.xRotationSpeed * Time.deltaTime;
+		barrel.Rotate(barrelRotX, 0, 0);
 	}
 
 	void FixedUpdate()
 	{
+		if(Input.GetButtonDown("Fire1"))
+			shellForce = shellProperties.initialForce;
+		else if(Input.GetButton("Fire1"))
+		{
+			shellForce += shellProperties.deltaForce;
+			if(shellForce.)
+		}
+		else if(Input.GetButtonUp("Fire1"))
+		{
+			GameObject shell = Instantiate(shellPrefab, transform.Find("Shell Spawn").position, barrel.rotation) as GameObject;
+			shell.GetComponent<ShellProjectile>().SetForce(shellForce);
+		}
+
 		//Move forward and backward
 		float move = Input.GetAxis("Vertical") * tankMoveSpeed;
-		rb.AddForce(transform.forward * move, ForceMode.Impulse);
-//		Debug.Log(rb.velocity.ToString());
+		rb.AddForce(transform.forward * move);
 
+		//Rotate tank body and counter the rotation for the turret head
 		float tankRotate = Input.GetAxis("Horizontal") * yRotationSpeed;
-		transform.Rotate(transform.up * tankRotate);
+		transform.Rotate(0, tankRotate, 0);
+		turret.Rotate(0, -tankRotate, 0);
+		Camera.main.transform.RotateAround(turret.position, turret.up, -tankRotate);
 	}
 }
